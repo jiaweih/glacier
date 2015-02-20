dbeeda979d2bef80682eb4e13f421ae4e20f60c8
jiaweih <jiaweihe@Jiaweis-MBP.PK5001Z>
2015-02-20 00:10:28 -0800

    start using cython

--- glacier.py
+++ glacier.py
@@ -8,6 +8,9 @@ import time
 import datetime as datetime
 import scipy.io as io
 from scipy.sparse import csr_matrix
+from scipy.sparse import linalg
+import matlab_wrapper
+import solver
 
 # mat = scipy.io.loadmat(infile)
 # B = mat['B']
@@ -45,10 +48,12 @@ def main():
 	N = nx * ny
 
 	# size = np.size(B)
-	B = B.reshape(N,1)  
+	# B = B.T.reshape(N,1)  
+	B = B.T.flatten()
 	B[np.isnan(B)] = 0
 
-	b_dot = b_dot.reshape(N,1)
+	# b_dot = b_dot.T.reshape(N,1)
+	b_dot = b_dot.T.flatten()
 	S = B
 
 	### Display some diagnostics before proceeding with model integration
@@ -56,38 +61,45 @@ def main():
 	t_SAVE = dt_SAVE
 	tm = time.clock()
 
-	########
-	# nx = 6
-	# ny = 7
-
 	while 1:
-		S,t,LAMBDA_max,k_LAMBDA_max = step(S, B, b_dot, dt, N, t, nx, ny, dx, METHOD)
-		H_max = np.max(S-B)
-		k_H_max = np.argmax(S-B)
-		S_max = np.max(S)
-		k_S_max = np.argmax(S)
+		# np.savetxt('H.txt',S)
+		# print S[100:105]
+		# S,t,LAMBDA_max,k_LAMBDA_max = step(S, B, b_dot, dt, N, t, nx, ny, dx, METHOD)
+		S,t = step(S, B, b_dot, dt, N, t, nx, ny, dx, METHOD)
+		# print 'Starting to calculate max'
+		# H_max = np.max(S-B)
+		# print 'H_max done'
+		# k_H_max = np.argmax(S-B)
+		# print 'k_H_max: {}'.format(k_H_max)
+		# S_max = np.max(S)
+		# print 'S_max'
+		# k_S_max = np.argmax(S)
+		# print 'k_S_max:  {}'.format(k_S_max)
+		H_max,k_H_max,S_max,k_S_max = solver.find_max(S,B)
 		# H_max, k_H_max = np.max(S - B)  ##### @matlab
 		# S_max, k_S_max = np.max(S)        ##### @matlab
+		# print 'S_max={}'.format(S_max)
+		# print 'k_S_max={}'.format(k_S_max)
+		# print S
 
 		ALPHA_I = 100*np.sum(S>B)/N
 		
-		print 'BKS: At t={:8.2f} yr ALPHA_I={:.2f}%% and maxima are: H({:d}) = {:f} \
-				S({:d})={:f} LAMBDA({:d}) = {:f}\n'.format(t, ALPHA_I, k_H_max, H_max, k_S_max, S_max, k_LAMBDA_max, LAMBDA_max)
+		print 'BKS: At t={:8.2f} yr ALPHA_I={:.2f}% and maxima are: H({:d}) = {:f} S({:d})={:f}\n'.format(t, ALPHA_I, k_H_max, H_max, k_S_max, S_max)
 
 		if t > t_STOP:
 			break
 		
-		I = np.zeros((ny,nx))
-		I[S>B] = 1
+		# I = np.zeros((ny,nx))
+		# I[S>B] = 1
 		
-		plt.imshow(I)
-		plt.title('Location: {:s}; t = {:.2f} yr'.format(run_str, t))
-		file_str = '{:s}_{:f}'.format(run_str, round(t))
+		# plt.imshow(I)
+		# plt.title('Location: {:s}; t = {:.2f} yr'.format(run_str, t))
 
-		S_map = S.reshape(ny,nx)
-		B_map = B.reshape(ny,nx)
-		I_map = I.reshape(ny,nx)
+		# S_map = S.reshape(ny,nx)
+		# B_map = B.reshape(ny,nx)
+		# I_map = I.reshape(ny,nx)
 		now = datetime.datetime.now().strftime('%H:%M:%S')
+		file_str = '{:s}_{:f}'.format(run_str, round(t))
 		print 'main(): Output stored in file "{:s}" at time {:s} \n'.format('{}.mat'.format(file_str),now)
 		
 		t_SAVE = t_SAVE + dt_SAVE
@@ -95,6 +107,217 @@ def main():
 	e = time.clock() - tm  ### @matlab
 	print 'ALL DONE: Forward modelling'
 
+
+def step(S, B, b_dot, dt, N, t, nx, ny, dx, METHOD):
+	# A_tilde = np.empty
+	# if A_tilde.size == 0:
+		# A_tilde,C_tilde,nm_half,npl,mm_half,ml = isempty_A_tilde(A_GLEN,RHO,g,n_GLEN,dx,C_SLIDE,m_SLIDE)
+	# t_n = t + dt
+	# S_out = np.genfromtxt('S_out.txt')
+	# return S_out,t_n
+	ic_jc,im_jc,ip_jc,ic_jm,ic_jp,im_jm,ip_jm,im_jp,ip_jp = SetupIndexArrays(nx,ny) 
+	D_IC_jc, D_IP_jc, D_ic_JC, D_ic_JP = diffusion_gl(S,B,nx,ny,dx)
+	D_sum = D_IC_jc + D_IP_jc + D_ic_JC + D_ic_JP
+	
+	row = np.int32([[ic_jc],[ic_jc],[ic_jc],[ic_jc],[ic_jc]]).flatten()
+	# row = row.T.reshape(row.size,1).T
+	col = np.int32([[im_jc],[ip_jc],[ic_jm],[ic_jp],[ic_jc]]).flatten()
+	# col = col.T.reshape(col.size,1).T
+	val = np.array([[-OMEGA*D_IC_jc],[-OMEGA*D_IP_jc],[-OMEGA*D_ic_JC],[-OMEGA*D_ic_JP],[1/dt+OMEGA*D_sum]]).flatten()
+	# np.savetxt('val.txt',val)
+	# row = row - 1
+	# col = col - 1
+	# print 'row={}'.format(row)
+	# print 'col={}'.format(col)
+	# np.savetxt('row.txt',row)
+	# np.savetxt('col.txt',col)
+	# A = csr_matrix( (val,(row,col)), shape=(N,N)).todense()   ### matrix A is symmetric positive definite
+	# A = csr_matrix( (val,(row,col)), shape=(N,N))
+	C = (1 - OMEGA) * ((D_IC_jc * S[im_jc]) + D_IP_jc * S[ip_jc] + D_ic_JC * S[ic_jm] + D_ic_JP * S[ic_jp]) + (1/dt - (1 - OMEGA) * D_sum) * S[ic_jc] + b_dot 
+	C = C.flatten()
+	# np.savetxt('A.txt',A)
+	# np.savetxt('C.txt',C)
+	# print A[2,1]
+	# print A[2,2]
+	# print A[2,3]
+	# print row[-10:-1]
+	# print col[0:10]
+	print 'starting to solve'
+	
+	# S_out = solver(row,col,val,N,C,METHOD) 
+	S_out = solver.solver(row,col,val,N,C,METHOD)
+	# print S_out[0:10]
+	# return S_out
+	print 'solved'
+	# print S_out[0:10]
+	S_out = solver.compareSB(S_out,B)
+	# S_out[S_out<B] = B[B>S_out]
+	# np.savetxt('S_out.txt',S_out)
+	print S_out[0:10]
+	
+	# H_out = S_out - B
+	t_n = t + dt
+	
+	# D_max = np.max(D_IC_jc+D_IP_jc+D_ic_JC+D_ic_JP)
+	# k_LAMBDA_max = np.argmax(D_IC_jc+D_IP_jc+D_ic_JC+D_ic_JP)
+	# # D_max,k_LAMBDA_max = np.max(D_IC_jc+D_IP_jc+D_ic_JC+D_ic_JP)  ######### @matlab
+	# LAMBDA_max = 0.25 * dt * D_max
+	
+	return S_out,t_n
+
+# def compareSB(S_out,B):
+# 	size = len(S_out)
+# 	for i in np.arange(size):
+# 		if S_out[i] < B[i]:
+# 			S_out[i] = B[i]
+# 	# S_out[S_out<B] = B[B>S_out]
+# 	return S_out
+
+def diffusion_gl(S,B,nx,ny,dx):
+	A_tilde = 2*A_GLEN*(RHO*g)**n_GLEN/(n_GLEN+2)/dx**2
+	C_tilde = C_SLIDE*(RHO*g)**m_SLIDE/dx**2
+	nm_half = (n_GLEN-1)/2
+	npl = n_GLEN+1
+	mm_half = (m_SLIDE-1)/2
+	ml = m_SLIDE
+	
+	# SB = S-B
+	SB = S
+	SB[SB<0] = 0
+	H = SB
+	# np.savetxt('H.txt',S)
+	ic_jc,im_jc,ip_jc,ic_jm,ic_jp,im_jm,ip_jm,im_jp,ip_jp = SetupIndexArrays(nx,ny) 
+
+
+	# np.savetxt('ic_jc.txt',ic_jc)
+	# np.savetxt('im_jc.txt',im_jc)
+	
+	H_IC_jc = 0.5*(H[ic_jc] + H[im_jc])
+	H_ic_JC = 0.5*(H[ic_jc] + H[ic_jm])
+
+	# np.savetxt('H_IC_jc.txt',H_IC_jc)
+	
+	H_IC_jc_up = H[im_jc]
+	H_ic_JC_up = H[ic_jm]
+	
+	ix = (S[ic_jc]>S[im_jc]).reshape(-1)
+	H_IC_jc_up[S[ic_jc]>S[im_jc]] = H[ic_jc[ix]].reshape(-1)
+	# np.savetxt('H_IC_jc_up.txt',H_IC_jc_up)
+
+	ix = (S[ic_jc]>S[ic_jm]).reshape(-1)
+	H_ic_JC_up[S[ic_jc]>S[ic_jm]] = H[ic_jc[ix]].reshape(-1)
+	
+	dS_dx_IC_jc = (S[ic_jc]-S[im_jc])/dx
+	dS_dy_IC_jc = (S[ic_jp]+S[im_jp]-S[ic_jm]-S[im_jm])/(4*dx)
+	dS_dx_ic_JC = (S[ip_jc]+S[ip_jm]-S[im_jc]-S[im_jm])/(4*dx)
+	dS_dy_ic_JC = (S[ic_jc]-S[ic_jm])/dx
+
+	# np.savetxt('dS_dx_IC_jc.txt',dS_dx_IC_jc)
+	
+	S2_IC_jc = np.square(dS_dx_IC_jc) + np.square(dS_dy_IC_jc) + K_eps
+	S2_ic_JC = np.square(dS_dx_ic_JC) + np.square(dS_dy_ic_JC) + K_eps
+	# np.savetxt('S2_IC_jc.txt',S2_IC_jc)
+	
+	if C_tilde == 0:    ### No sliding case
+		D_IC_jc = A_tilde*H_IC_jc_up*np.power(H_IC_jc,npl)*np.power(S2_IC_jc,nm_half)
+		D_ic_JC = A_tilde*H_ic_JC_up*np.power(H_ic_JC,npl)*np.power(S2_ic_JC,nm_half)
+	elif C_tilde > 0:    ### Sliding case
+		D_IC_jc = A_tilde*H_IC_jc_up*np.power(H_IC_jc,npl)*np.power(S2_IC_jc,nm_half) \
+				+ C_tilde*H_IC_jc_up*np.power(H_IC_jc,ml)*np.power(S2_IC_jc,mm_half)
+		D_ic_JC = A_tilde*H_ic_JC_up*np.power(H_ic_JC,npl)*np.power(S2_ic_JC,nm_half) \
+				+ C_tilde*H_ic_JC_up*np.power(H_ic_JC,ml)*np.power(S2_ic_JC,mm_half)
+	else:
+		print 'diffusion(): C_tilde is undefined or incorrectly defined'
+		
+	D_IP_jc  = D_IC_jc[ip_jc]
+	D_ic_JP  = D_ic_JC[ic_jp]
+	# np.savetxt('D_IP_jc.txt',D_IP_jc)
+	
+	return D_IC_jc,D_IP_jc,D_ic_JC,D_ic_JP
+
+
+def SetupIndexArrays(nx, ny):
+	N = nx * ny
+
+	ic_jc = np.arange(1,N+1)  
+	ic_jc = ic_jc.reshape(nx,ny)
+
+	ic = np.arange(nx)
+	ip = np.append(np.array([range(1,nx)]),nx - 1)
+	im = np.append(0,np.array([range(nx - 1)]))
+
+	jc = np.arange(ny)
+	jp = np.append(0,np.array([range(ny - 1)]))
+	jm = np.append(np.array([range(1,ny)]),ny - 1)
+
+	ip_jc = setupArrays(ip,jc,ic_jc) - 1
+	im_jc = setupArrays(im,jc,ic_jc) - 1
+	ic_jp = setupArrays(ic,jp,ic_jc) - 1
+	ic_jm = setupArrays(ic,jm,ic_jc) - 1
+
+	im_jm = setupArrays(im,jm,ic_jc) - 1
+	ip_jm = setupArrays(ip,jm,ic_jc) - 1
+	im_jp = setupArrays(im,jp,ic_jc) - 1
+	ip_jp = setupArrays(ip,jp,ic_jc) - 1
+
+	ic_jc = ic_jc.reshape(-1) - 1
+
+	return ic_jc,im_jc,ip_jc,ic_jm,ic_jp,im_jm,ip_jm,im_jp,ip_jp
+
+
+def setupArrays(a,b,ic_jc):
+	x,y = np.meshgrid(b,a)
+	array = []
+	for l in zip(y.ravel(),x.ravel()):
+		array.append(ic_jc[l])
+	array = np.array(array)
+	return array
+
+
+# def solver(row,col,val,N,C,METHOD):
+# 	upper = str.upper(METHOD)
+# 	A = csr_matrix( (val,(row,col)), shape=(N,N))
+# 	if upper == 'BACKSLASH':
+# 		# matlab = matlab_wrapper.MatlabSession()
+# 		print 'starting to solve'
+# 		# matlab.put('A',A)
+# 		# matlab.put('C',C)
+# 		# matlab.eval('x = A\C')
+# 		# S_out = matlab.get('x')
+# 		S_out = linalg.spsolve(A,C)
+# 		#S_out = np.linalg.lstsq(A,C)
+# 		#S_out = linalg.lsqr(A,C)
+# 		#S_out = np.linalg.solve(A,C)
+# 	elif upper == 'PCG':
+# 		tol = 1.0e-06       ### 1.0e-09;
+# 		MAXIT = 100
+# 		S_out = np.empty(N*N)
+# 		info, itern, relres = krylov.pcg(A, C, S_out, tol, MAXIT)
+# 		if info != 0:
+# 			if info == -1:
+# 				print 'step(): pcg did not converge after MAXIT iterations at t={:f} yr'.format(t)
+# 			elif info == -2:
+# 				print 'step(): pcg preconditioner is ill-conditioned at t={:f} yr'.format(t)
+# 			elif info == -5:
+# 				print 'step(): pcg stagnated at t={:f} yr'.format(t)
+# 			elif info == -6:
+# 				print 'step(): one of the scalar quantities in pcg became too small or too large at t={:f} yr'.format(t)
+# 			else:
+# 				print 'step(): Unknown pcg flag raised, flat={}'.format(info)
+# 	return S_out
+
+def print_out(METHOD,OMEGA,dt,A_GLEN,C_SLIDE,nx,ny):
+    print '=================================================================================='
+    print 'LAUNCHING GLACIER SIMULATION MODEL - Ver 5.01 using the {:s} solver\n\n'.format(METHOD)
+    print '  OMEGA      = {:.2f}\n'.format(OMEGA)
+    print '  dt         = {:.2f} yr\n'.format(dt)
+    print '  A_GLEN     = {:e}\n'.format(A_GLEN)
+    print '  C_SLIDE    = {:e}\n'.format(C_SLIDE)
+    print '  nx         = {:d}\n'.format(nx)
+    print '  ny         = {:d}\n'.format(ny)
+    if METHOD.find('ADI') != -1:
+        print '  ADI METHOD = {%s}\n'.format(ADI_METHOD)
+    print '=================================================================================='
 
 def model_params(MODEL):
 	case = {0: case_0, 1: case_1, 2: case_2, 3: case_3, 4: case_4}
@@ -142,7 +365,7 @@ def case_0():
 	x_c = 0.5*L_x
 	y_c = 0.5*L_y
 	
-	X, Y = np.meshgrid(x,y)     ################# differ from @matlab?
+	X, Y = np.meshgrid(x,y)     
 	
 	Z0 = 2000
 	sigma_x = x_c
@@ -193,7 +416,7 @@ def case_3():
 	file_dat = '{}.mat'.format(run_str)
 	B,b_dot,dx,dy,ny,nx = read_mat(file_dat)
 	
-	t_STOP = 1000
+	t_STOP = 0.07        ### 1000
 	dt_SAVE = 5*t_STOP
 	dt = 0.08333
 	return B,b_dot,dx,dy,ny,nx,t_STOP,dt_SAVE,dt,run_str
@@ -219,175 +442,6 @@ def case_4():
 	if METHOD.find('ADI') != -1:
 		print '  ADI METHOD = {%s}\n'.format(ADI_METHOD)
 	print '=================================================================================='
-
-def step(S, B, b_dot, dt, N, t, nx, ny, dx, METHOD):
-	# A_tilde = np.empty
-	# if A_tilde.size == 0:
-		# A_tilde,C_tilde,nm_half,npl,mm_half,ml = isempty_A_tilde(A_GLEN,RHO,g,n_GLEN,dx,C_SLIDE,m_SLIDE)
-	ic_jc,im_jc,ip_jc,ic_jm,ic_jp,im_jm,ip_jm,im_jp,ip_jp = SetupIndexArrays(nx,ny) 
-
-	D_IC_jc, D_IP_jc, D_ic_JC, D_ic_JP = diffusion_gl(S,B,nx,ny,dx)
-	D_sum = D_IC_jc + D_IP_jc + D_ic_JC + D_ic_JP
-	
-	row = np.array([[ic_jc],[ic_jc],[ic_jc],[ic_jc],[ic_jc]]).reshape(-1)
-	col = np.array([[im_jc],[ip_jc],[ic_jm],[ic_jp],[ic_jc]]).reshape(-1)
-	val = np.array([[-OMEGA*D_IC_jc],[-OMEGA*D_IP_jc],[-OMEGA*D_ic_JC],[-OMEGA*D_ic_JP],[1/dt+OMEGA*D_sum]]).reshape(-1)
-	
-	# row = row - 1
-	# col = col - 1
-
-	A = csr_matrix( (val,(row,col)), shape=(N,N)).todense()   ### matrix A is symmetric positive definite
-	C = (1 - OMEGA) * ((D_IC_jc * S[im_jc]) + D_IP_jc * S[ip_jc] + D_ic_JC * S[ic_jm] + D_ic_JP * S[ic_jp]) + (1/dt - (1 - OMEGA) * D_sum) * S[ic_jc] + b_dot 
-	
-	S_out = solver(A,C,METHOD)
-	S_out[S_out<B] = B[B>S_out]
-	
-	H_out = S_out - B
-	t_n = t + dt
-	
-	D_max = np.max(D_IC_jc+D_IP_jc+D_ic_JC+D_ic_JP)
-	k_LAMBDA_max = np.argmax(D_IC_jc+D_IP_jc+D_ic_JC+D_ic_JP)
-	# D_max,k_LAMBDA_max = np.max(D_IC_jc+D_IP_jc+D_ic_JC+D_ic_JP)  ######### @matlab
-	LAMBDA_max = 0.25 * dt * D_max
-	
-	return S,t,LAMBDA_max,k_LAMBDA_max
-
-
-def diffusion_gl(S,B,nx,ny,dx):
-	A_tilde = 2*A_GLEN*(RHO*g)**n_GLEN/((n_GLEN+2)*dx**2)
-	C_tilde = C_SLIDE*(RHO*g)**m_SLIDE/dx**2
-	nm_half = (n_GLEN-1)/2
-	npl = n_GLEN+1
-	mm_half = (m_SLIDE-1)/2
-	ml = m_SLIDE
-	
-	SB = S-B
-	SB[SB<0] = 0
-	H = SB
-
-	ic_jc,im_jc,ip_jc,ic_jm,ic_jp,im_jm,ip_jm,im_jp,ip_jp = SetupIndexArrays(nx,ny) 
-
-	print 'ic_jc: {}'.format(ic_jc)
-	print 'im_jc: {}'.format(im_jc)
-	
-	H_IC_jc = 0.5*(H[ic_jc] + H[im_jc])
-	H_ic_JC = 0.5*(H[ic_jc] + H[ic_jm])
-	
-	H_IC_jc_up = H[im_jc]
-	H_ic_JC_up = H[ic_jm]
-	
-	H_IC_jc_up = H[im_jc]
-	H_ic_JC_up = H[ic_jm]
-	
-	# print 'H:'
-	# print H
-	# print 'S:'
-	# print S
-	# print np.shape(S[ic_jc] > S[im_jc])
-	# print H_IC_jc_up[S[ic_jc]>S[im_jc]]
-	# print H_IC_jc_up.shape
-	ix = (S[ic_jc]>S[im_jc]).reshape(-1)
-	H_IC_jc_up[S[ic_jc]>S[im_jc]] = H[ic_jc[ix]].reshape(-1)
-	ix = (S[ic_jc]>S[ic_jm]).reshape(-1)
-	H_ic_JC_up[S[ic_jc]>S[ic_jm]] = H[ic_jc[ix]].reshape(-1)
-	
-	dS_dx_IC_jc = (S[ic_jc]-S[im_jc])/dx
-	dS_dy_IC_jc = (S[ic_jp]+S[im_jp]-S[ic_jm]-S[im_jm])/(4*dx)
-	dS_dx_ic_JC = (S[ip_jc]+S[ip_jm]-S[im_jc]-S[im_jm])/(4*dx)
-	dS_dy_ic_JC = (S[ic_jc]-S[ic_jm])/dx
-	
-	S2_IC_jc = np.square(dS_dx_IC_jc) + np.square(dS_dy_IC_jc) + K_eps
-	S2_ic_JC = np.square(dS_dx_ic_JC) + np.square(dS_dy_ic_JC) + K_eps
-	
-	if C_tilde == 0:    ### No sliding case
-		D_IC_jc = A_tilde*H_IC_jc_up*np.power(H_IC_jc,npl)*np.power(S2_IC_jc,nm_half)
-		D_ic_JC = A_tilde*H_ic_JC_up*np.power(H_ic_JC,npl)*np.power(S2_ic_JC,nm_half)
-	elif C_tilde > 0:    ### Sliding case
-		D_IC_jc = A_tilde*H_IC_jc_up*np.power(H_IC_jc,npl)*np.power(S2_IC_jc,nm_half) \
-				+ C_tilde*H_IC_jc_up*np.power(H_IC_jc,ml)*np.power(S2_IC_jc,mm_half)
-		D_ic_JC = A_tilde*H_ic_JC_up*np.power(H_ic_JC,npl)*np.power(S2_ic_JC,nm_half) \
-				+ C_tilde*H_ic_JC_up*np.power(H_ic_JC,ml)*np.power(S2_ic_JC,mm_half)
-	else:
-		print 'diffusion(): C_tilde is undefined or incorrectly defined'
-		
-	D_IP_jc  = D_IC_jc[ip_jc]
-	D_ic_JP  = D_ic_JC[ic_jp]
-	
-	return D_IC_jc,D_IP_jc,D_ic_JC,D_ic_JP
-
-
-def SetupIndexArrays(nx, ny):
-	N = nx * ny
-
-	ic_jc = np.arange(1,N+1)  
-	ic_jc = ic_jc.reshape(nx,ny)
-
-	ic = np.arange(nx)
-	ip = np.append(np.array([range(1,nx)]),nx - 1)
-	im = np.append(0,np.array([range(nx - 1)]))
-
-	jc = np.arange(ny)
-	jp = np.append(0,np.array([range(ny - 1)]))
-	jm = np.append(np.array([range(1,ny)]),ny - 1)
-
-	ip_jc = setupArrays(ip,jc,ic_jc) - 1
-	im_jc = setupArrays(im,jc,ic_jc) - 1
-	ic_jp = setupArrays(ic,jp,ic_jc) - 1
-	ic_jm = setupArrays(ic,jm,ic_jc) - 1
-
-	im_jm = setupArrays(im,jm,ic_jc) - 1
-	ip_jm = setupArrays(ip,jm,ic_jc) - 1
-	im_jp = setupArrays(im,jp,ic_jc) - 1
-	ip_jp = setupArrays(ip,jp,ic_jc) - 1
-
-	ic_jc = ic_jc.reshape(-1) - 1
-
-	return ic_jc,im_jc,ip_jc,ic_jm,ic_jp,im_jm,ip_jm,im_jp,ip_jp
-
-
-def setupArrays(a,b,ic_jc):
-	x,y = np.meshgrid(b,a)
-	array = []
-	for l in zip(y.ravel(),x.ravel()):
-		array.append(ic_jc[l])
-	array = np.array(array)
-	return array
-
-
-def solver(A,C,METHOD):
-	upper = str.upper(METHOD)
-	if upper == 'BACKSLASH':
-		S_out = np.linalg.solve(A,C)
-	elif upper == 'PCG':
-		tol = 1.0e-06       ### 1.0e-09;
-		MAXIT = 100
-		S_out = np.empty(N*N)
-		info, itern, relres = krylov.pcg(A, C, S_out, tol, MAXIT)
-		if info != 0:
-			if info == -1:
-				print 'step(): pcg did not converge after MAXIT iterations at t={:f} yr'.format(t)
-			elif info == -2:
-				print 'step(): pcg preconditioner is ill-conditioned at t={:f} yr'.format(t)
-			elif info == -5:
-				print 'step(): pcg stagnated at t={:f} yr'.format(t)
-			elif info == -6:
-				print 'step(): one of the scalar quantities in pcg became too small or too large at t={:f} yr'.format(t)
-			else:
-				print 'step(): Unknown pcg flag raised, flat={}'.format(info)
-	return S_out
-
-def print_out(METHOD,OMEGA,dt,A_GLEN,C_SLIDE,nx,ny):
-    print '=================================================================================='
-    print 'LAUNCHING GLACIER SIMULATION MODEL - Ver 5.01 using the {:s} solver\n\n'.format(METHOD)
-    print '  OMEGA      = {:.2f}\n'.format(OMEGA)
-    print '  dt         = {:.2f} yr\n'.format(dt)
-    print '  A_GLEN     = {:e}\n'.format(A_GLEN)
-    print '  C_SLIDE    = {:e}\n'.format(C_SLIDE)
-    print '  nx         = {:d}\n'.format(nx)
-    print '  ny         = {:d}\n'.format(ny)
-    if METHOD.find('ADI') != -1:
-        print '  ADI METHOD = {%s}\n'.format(ADI_METHOD)
-    print '=================================================================================='
 
 ### if A_tilde.size == 0, then execute the following function
 def isempty_A_tilde(A_GLEN,RHO,g,n_GLEN,dx,C_SLIDE,m_SLIDE):
